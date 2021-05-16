000a7f6c558f06dbeefb0886bcc0aa89a9882deb
Steven Ulibarri <steven.ulibarri@gmail.comm>
2021-05-16 12:19:47 -0600

    renaming

--- test/DataRepository.test.ts
+++ test/DataRepository.test.ts
@@ -3,20 +3,19 @@ import { DbConnector } from '../src/DbConnector';
 
 jest.mock('../src/DbConnector');
 
-describe('DataRetriever', () => {
+describe('DataRepository', () => {
   
   test('mock', async () => {
-    
     // note usage of jest.Mocked vs jest.Mock
     // jest.Mocked enables type inference for the class methods
     // as jest.Mock is so the checker won't complain about missing constructor args
     const mockDbConnector: jest.Mocked<DbConnector> = new (DbConnector as jest.Mock)();
-    const retriever: IDataRepository = new DataRepository(mockDbConnector);
+    const repo: IDataRepository = new DataRepository(mockDbConnector);
 
     // autocomplete actually works here! and the return type is enforced!
     mockDbConnector.query.mockResolvedValue(['some', 'mocked', 'data']);
 
-    const result = await retriever.getDataById(123);
+    const result = await repo.getDataById(123);
 
     expect(result.length).toBe(3);
     expect(result).toEqual(expect.arrayContaining(['some', 'mocked', 'data']));
